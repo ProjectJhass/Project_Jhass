40840a27ca536d51c9da87d77a31f24430e74997
Martiinhz <once11caldas.martin@gmail.com>
2024-06-25 16:50:48 -0500

    Creacion tercera seccion inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Header } from './components/Layouts/Header/Header'
-import Section1 from './components/Sections/Section-1/Section1'
+import  Section1 from './components/Sections/Section-1/Section1'
 import  Section2  from "./components/Sections/Section-2/Section2";
+import  {Section3}  from "./components/Sections/Section-3/Section3";
+
 
 
 function App() {
 
   return (
     <>
-<Section1/>
-<Section2/>
+{/* <Section1/>
+<Section2/> */}
+<Section3/>
     </>
   )
 }
