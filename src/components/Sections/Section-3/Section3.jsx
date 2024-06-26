b99bc54fcccc631c937b213c1f1c6753a9451a25
Martiinhz <once11caldas.martin@gmail.com>
2024-06-25 19:54:52 -0500

    Algunos arreglos generales

--- src/components/Sections/Section-3/Section3.jsx
+++ src/components/Sections/Section-3/Section3.jsx
@@ -6,6 +6,9 @@ import { Cards } from '../../Cards/Cards'
 export const Section3 = () => {
   return (
     <>
+    <h1>El CRM que potencia tu empresa y facilita la gestión de empleados</h1>
+    <p>JHASS es un CRM diseñado para optimizar la productividad y simplificar la gestión de recursos humanos en tu empresa. Ofrece una plataforma intuitiva que organiza datos y mejora la comunicación y eficiencia operativa, llevando tu negocio al siguiente nivel.</p>
+
     <div className="flex flex-wrap gap-4">
       {Cards.map((card, index) => (
         <Card
