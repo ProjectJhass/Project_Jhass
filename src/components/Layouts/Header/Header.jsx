640858739071b631aaf6beff6b28b76e7fbadc48
Martiinhz <once11caldas.martin@gmail.com>
2024-06-25 22:59:59 -0500

    Arreglando cositas

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -3,10 +3,10 @@ import img1 from '../../../images/Logo3.png';
 
 export const Header = () => {
   return (
-    <header className="bg-[#EEEEEE] shadow-md mx-8  rounded-lg">
+    <header className="sticky top-0 z-50 bg-[#EEEEEE] shadow-md mx-8 rounded-lg">
       <div className="container mx-auto flex items-center justify-evenly py-4 text-2xl">
         <div className="flex items-center">
-          <img src={img1} alt="Logo" className="h-10 w-14 " />
+          <img src={img1} alt="Logo" className="h-10 w-14" />
         </div>
         <nav>
           <ul className="flex space-x-8">
@@ -16,7 +16,7 @@ export const Header = () => {
           </ul>
         </nav>
         <div>
-          <button className="bg-blue-500 text-white text-xl rounded-lg px-0.5 py-0.5 hover:bg-[#0165FF]">Iniciar Sesión</button>
+          <button className="bg-blue-500 text-white text-xl rounded-lg px-2 py-1 hover:bg-[#0165FF]">Iniciar Sesión</button>
         </div>
       </div>
     </header>
